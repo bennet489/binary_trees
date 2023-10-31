ba6eabbcf6b83fbd4d0eecc2f73c3bf09beb7d99
Gold Israel <techiegold101@gmail.com>
2023-10-31 17:07:13 +0100

    remove the else statement, to avoid betty warning.. all same

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,8 +14,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	else if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
-	{
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-	}
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
