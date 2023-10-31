32795e90c0db9cd57b5fe119ad29daf20af43c64
Gold Israel <techiegold101@gmail.com>
2023-10-31 17:36:26 +0100

    implement betty style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,22 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes -  counts the nodes with at least 1 child in a binary tree.
+ * binary_tree_nodes -  counts the nodes with at least 1
+ * child in a binary tree.
  * @tree: A  pointer to the root node of the tree to count the number of nodes.
- * 
+ *
  * Return:  the number of nodes with at least 1 child in a binary tree,
  * or NULL if tree if empty.
 */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t left_count, right_count;
+	size_t left_count;
+	size_t right_count;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
+	left_count = binary_tree_nodes(tree->left);
+	right_count = binary_tree_nodes(tree->right);
 
-    if (tree->left || tree->right )
-        return (left_count + right_count + 1);
-    else
-        return (left_count + right_count);
+	if (tree->left || tree->right)
+		return (left_count + right_count + 1);
+	else
+		return (left_count +  right_count);
 }
