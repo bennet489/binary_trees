c491423fa90021a1983599b067610aa6a46889ed
Gold Israel <techiegold101@gmail.com>
2023-10-31 15:39:44 +0100

    use recursive method and also correct betty styling

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,28 +3,18 @@
 /**
  * binary_tree_depth - Measure the depth of a node in a binary tree.
  * @tree: A pointer to the node for which the depth is to be measured.
- * 
+ *
  * Return: The depth of the specified node in the binary tree,
  *  or 0 if the node is NULL.
 */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
-    const binary_tree_t *current = tree;
+	if (tree == NULL)
+		return (0);
 
-    if (tree == NULL)
-        return 0;
-
-    while (current)
-    {
-        depth++;
-        current = current->left;
-        
-        if (current == NULL)
-        {
-              current = tree->right;
-        }
-    }
-    return (depth);
+	else
+	{
+		return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	}
 }
