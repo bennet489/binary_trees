e801836a8d29134ef67fcb5fb9a3a44ff2861df3
Gold Israel <techiegold101@gmail.com>
2023-11-01 13:41:05 +0100

    add func that measures the balance factor of a binary tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			right_height = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+}
+
+/**
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right = 0, left = 0, total = 0;
+
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
+	}
+	return (total);
+}
