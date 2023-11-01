ef301e4666b9af4d59a473a3d59e59c8cbccca0a
Gold Israel <techiegold101@gmail.com>
2023-11-01 14:32:19 +0100

    update by adding more if else condintionals

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,22 +1,37 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_full - checks if a binary tree is full.
- * @tree: Pointer to the root node of the tree to check.
- *
- * Return: return 1 if the tree is full, else 0.
-*/
-
+ * binary_tree_is_full - function that says if a tree is full or not
+ * a tree is full if it has two or none children
+ * @tree: tree to check
+ * Return: 1 if it is a full 0 if not
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	if ((tree->left == NULL && tree->right != NULL) ||
-	(tree->left != NULL && tree->right == NULL)
-		return (0);
+	int left = 0, right = 0;
 
-	return (binary_tree_is_full(tree->left)
-	&& binary_tree_is_full(tree->right));
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree->left && tree->right)
+		{
+			left = binary_tree_is_full(tree->left);
+			right = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		else if (!tree->left && !tree->right)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
 }
